3f07079f8abf8c091eb89d8e1326af343da6e363
AxellinoAnggoro <axellinoanggoro1311@gmail.com>
2023-03-13 22:28:44 +0700

    add latihan button

--- app/src/main/java/binar/academy/myapplication/HomeActivity.kt
+++ app/src/main/java/binar/academy/myapplication/HomeActivity.kt
@@ -2,9 +2,10 @@ package binar.academy.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-class MainActivity : AppCompatActivity() {
+
+class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_home)
     }
 }
